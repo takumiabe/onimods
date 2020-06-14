4968e17e8bc8774fb47ef5102d969a683396cd2d
Takumi Abe <abe@engraphia.com>
2020-06-14 11:21:47 +0900

    Add PauseOnReadyToPrint

---
+++ PauseOnReadyToPrint/Patches.cs
@@ -0,0 +1,20 @@
+﻿using Harmony;
+
+namespace PauseOnReadyToPrint
+{
+    [HarmonyPatch(typeof(Telepad.States))]
+    [HarmonyPatch("InitializeStates")]
+    public class Patches
+    {
+        public static void Postfix(Telepad.States __instance)
+        {
+            // Telepadの状態がopenに遷移したときに、ゲームを一時停止する
+            __instance.open.Enter("trap", delegate (Telepad.StatesInstance smi)
+            {
+                // 音は鳴らす (true)
+                SpeedControlScreen.Instance.Pause(true);
+            }
+            );
+        }
+    }
+}
