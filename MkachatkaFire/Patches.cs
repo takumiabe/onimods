55aec0ff588b6f2f3e6754f6edabb616a3295d75
Takumi Abe <abe@engraphia.com>
2020-07-18 22:22:07 +0900

    Add MkachatkaFire

---
+++ MkachatkaFire/Patches.cs
@@ -0,0 +1,45 @@
+﻿using Harmony;
+using STRINGS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MkachatkaFire
+{
+    [HarmonyPatch(typeof(Geyser))]
+    [HarmonyPatch("OnSpawn")]
+    public class Patches
+    {
+        private static readonly EventSystem.IntraObjectHandler<Geyser> OnRefreshUserMenuDelegate =
+            new EventSystem.IntraObjectHandler<Geyser>(delegate(Geyser component, object data)
+        {
+            if (!Game.Instance.SandboxModeActive) return;
+
+            var startButton = new KIconButtonMenu.ButtonInfo(
+                "status_item_toilet_needs_emptying",
+                "Force Eruption",
+                delegate () {
+                    ElementEmitter emitter = Traverse.Create(component.smi.master).Field("emitter").GetValue<ElementEmitter>();
+                    emitter.SetEmitting(true);
+                },
+                global::Action.NumActions, null, null, null, "Start the eruption immediately", true);
+            Game.Instance.userMenu.AddButton(component.gameObject, startButton, 1f);
+
+            var stopButton = new KIconButtonMenu.ButtonInfo(
+                "status_item_toilet_needs_emptying",
+                "Stop Eruption",
+                delegate () {
+                    ElementEmitter emitter = Traverse.Create(component.smi.master).Field("emitter").GetValue<ElementEmitter>();
+                    emitter.SetEmitting(false);
+                },
+                global::Action.NumActions, null, null, null, "Stop the eruption immediately", true);
+            Game.Instance.userMenu.AddButton(component.gameObject, stopButton, 1f);
+        });
+
+        private static void Postfix(Geyser __instance)
+        {
+            __instance.Subscribe<Geyser>(493375141, OnRefreshUserMenuDelegate);
+        }
+    }
+}
