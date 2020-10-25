1bc3ff010b40cbfccc128485c9b7f366a80bd79a
Takumi Abe <abe@engraphia.com>
2020-10-26 00:04:33 +0900

    Revert "Merge branch 'feature/configurable_pause_on_ready_to_print'"
    This reverts commit 7dfd6cb764550fcb63dcabd30d486df530f6d1c3, reversing
    changes made to 55aec0ff588b6f2f3e6754f6edabb616a3295d75.

--- PauseOnReadyToPrint/Patches.cs
+++ PauseOnReadyToPrint/Patches.cs
@@ -1,55 +1,20 @@
 ﻿using Harmony;
-using UnityEngine;
 
 namespace PauseOnReadyToPrint
 {
+    [HarmonyPatch(typeof(Telepad.States))]
+    [HarmonyPatch("InitializeStates")]
     public class Patches
     {
-
-        [HarmonyPatch(typeof(HeadquartersConfig))]
-        [HarmonyPatch("ConfigureBuildingTemplate")]
-        public static class HeadquartersConfig_ConfigureBuildingTemplate
+        public static void Postfix(Telepad.States __instance)
         {
-            public static void Postfix(GameObject go, Tag prefab_tag)
+            // Telepadの状態がopenに遷移したときに、ゲームを一時停止する
+            __instance.open.Enter("trap", delegate (Telepad.StatesInstance smi)
             {
-                LogicAlarm alarm = go.AddOrGet<LogicAlarm>();
-                alarm.notificationName = STRINGS.CODEX.HEADQUARTERS.TITLE;
-                alarm.notificationTooltip = STRINGS.BUILDING.STATUSITEMS.NEWDUPLICANTSAVAILABLE.NAME;
-                alarm.pauseOnNotify = true;
-                alarm.zoomOnNotify = false;
+                // 音は鳴らす (true)
+                SpeedControlScreen.Instance.Pause(true);
             }
-        }
-
-        [HarmonyPatch(typeof(Telepad.States))]
-        [HarmonyPatch("InitializeStates")]
-        public static class TelepadStates_InitializeStates
-        {
-            public static void Postfix(Telepad.States __instance)
-            {
-                // Telepadの状態がopenに遷移したときに、内部的なLogicAlarmにON信号を送る
-                __instance.open.Enter(delegate (Telepad.StatesInstance smi)
-                {
-                    LogicAlarm alarm = smi.GetComponent<LogicAlarm>();
-                    if (alarm == null)
-                    {
-                        Debug.Log("LogicAlarm not found for telepad...");
-                        return;
-                    }
-                    alarm.OnLogicValueChanged(new LogicValueChanged { newValue = 1, portID = LogicAlarm.INPUT_PORT_ID });
-                });
-
-                // Telepadの状態がcloseに遷移したときに、内部的なLogicAlarmにOFF信号を送る
-                __instance.close.Enter(delegate (Telepad.StatesInstance smi)
-                {
-                    LogicAlarm alarm = smi.GetComponent<LogicAlarm>();
-                    if (alarm == null)
-                    {
-                        Debug.Log("LogicAlarm not found for telepad...");
-                        return;
-                    }
-                    alarm.OnLogicValueChanged(new LogicValueChanged { newValue = 0, portID = LogicAlarm.INPUT_PORT_ID });
-                });
-            }
+            );
         }
     }
 }
